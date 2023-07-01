f4d50ef8cec8cba4b21d1a8c5646fdcf428c0cf8
Natalia Romero <natalia.romero_g@mail.udp.cl>
2023-06-30 23:47:29 -0400

    se implementa streamlit

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from dotenv import dotenv_values
 import streamlit as st
+import time
+import requests
+import json
 from judini.agent import Agent
 
+# se genera conexión a judini
 env_variables = dotenv_values('.env')
 
 api_key = env_variables['JUDINI_API_KEY']
@@ -9,6 +13,62 @@ agent_id = env_variables['AGENT_ID']
 
 agent = Agent(api_key, agent_id)
 
-prompt = 'Hola'
-response = agent.completion(prompt)
-print(response)
+# se genera front del chat
+
+st.title('Chat Entel 📱')
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("¿En que te puedo ayudar?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+
+        #Judini
+        url = 'https://playground.judini.ai/api/v1/agent/'+agent_id
+        headers = {"Content-Type": "application/json; charset=utf-8", "Authorization": "Bearer "+api_key}
+        data = {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+        }
+        response = requests.post(url, headers=headers, json=data, stream=True)
+        raw_data = ''
+        tokens = ''
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                raw_data = chunk.decode('utf-8').replace("data: ", '')
+                if raw_data != "":
+                    lines = raw_data.strip().splitlines()
+                    for line in lines:
+                        print(line)
+                        line = line.strip()
+                        if line and line != "[DONE]":
+                            try:
+                                json_object = json.loads(line) 
+                                result = json_object['data']
+                                full_response += result
+                                time.sleep(0.05)
+                                # Add a blinking cursor to simulate typing
+                                message_placeholder.markdown(full_response + "▌")
+                            except json.JSONDecodeError:
+                                print(f'Error al decodificar el objeto JSON en la línea: {line}')
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
