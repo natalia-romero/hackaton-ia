d52441d93e24d04e9a509152f557edb5b20556e8
Nicolas Chirino <nicolas.brch@gmail.com>
2023-07-01 11:18:51 -0400

    Telefonos CSV desde Scraper

--- scraper.py
+++ scraper.py
@@ -1,7 +1,11 @@
 import requests
 import re
-import json
+import csv
 from bs4 import BeautifulSoup
+
+# Archivo destino
+archivo_csv = "telefonos.csv"
+headers = ["marca", "nombre", "precio", "almacenamiento", "pantalla", "camara"]
 
 # Request HTTP
 url = "https://miportal.entel.cl/personas/catalogo/celulares"
@@ -13,75 +17,72 @@ soup = BeautifulSoup(response.content, 'html.parser')
 # Obtener el elemento con id "rootProductInfo_JSON"
 product_info = soup.find(id="rootProductInfo_JSON")
 
-if product_info:
-    # Obtener el contenido de texto de
-    text_content = product_info.get_text()
+# Obtener el contenido de texto de
+text_content = product_info.get_text()
 
-    # Obtener los indices donde aparecen los atributos
-    indices = []
-    inicio = 0
-    while True:
-        index = text_content.find("attributes\": {", inicio)
-        if index == -1:
-            break
-        indices.append(index)
-        inicio = index + 1
+# Obtener los indices donde aparecen los atributos
+indices = []
+inicio = 0
+while True:
+    index = text_content.find("attributes\": {", inicio)
+    if index == -1:
+        break
+    indices.append(index)
+    inicio = index + 1
 
-    # Separar por los indices anteriores y guardarlo en una lista
-    contents = []
-    for i in range(len(indices)):
-        start = indices[i]
-        end = indices[i + 1] if i + 1 < len(indices) else len(text_content)
-        contents.append(text_content[start:end])
+# Separar por los indices anteriores y guardarlo en una lista
+contents = []
+for i in range(len(indices)):
+    start = indices[i]
+    end = indices[i + 1] if i + 1 < len(indices) else len(text_content)
+    contents.append(text_content[start:end])
 
-    # Obtener los valores que necesitamos de cada elemento de la lista
-    marcas = []
-    nombres = []
-    precios = []
-    almacenamientos = []
-    pantallas = []
-    camaras = []
-    for content in contents:
-        marca = r'"brand": \["(.*?)"\]'
-        matches = re.findall(marca, content)
-        marcas.extend(matches)
+# Obtener los valores que necesitamos de cada elemento de la lista
+marcas = []
+nombres = []
+precios = []
+almacenamientos = []
+pantallas = []
+camaras = []
+for content in contents:
+    marca = r'"brand": \["(.*?)"\]'
+    matches = re.findall(marca, content)
+    marcas.extend(matches)
 
-        nombre = r'"displayName": \["(.*?)"\]'
-        matches = re.findall(nombre, content)
-        nombres.extend(matches)
+    nombre = r'"displayName": \["(.*?)"\]'
+    matches = re.findall(nombre, content)
+    nombres.extend(matches)
 
-        precio = r'"price.formatted": \["(.*?)"\]'
-        matches = re.findall(precio, content)
-        precios.extend(matches)
+    precio = r'"price.formatted": \["(.*?)"\]'
+    matches = re.findall(precio, content)
+    precios.extend(matches)
 
-        almacenamiento = r'"internal": \["(.*?)"\]'
-        matches = re.findall(almacenamiento, content)
-        almacenamientos.extend(matches)
+    almacenamiento = r'"internal": \["(.*?)"\]'
+    matches = re.findall(almacenamiento, content)
+    almacenamientos.extend(matches)
 
-        pantalla = r'"product.screenSize": \["(.*?)"\]'
-        matches = re.findall(pantalla, content)
-        pantallas.extend(matches)
+    pantalla = r'"product.screenSize": \["(.*?)"\]'
+    matches = re.findall(pantalla, content)
+    pantallas.extend(matches)
 
-        camara = r'"primaryFromProduct": \["(.*?)"\]'
-        matches = re.findall(camara, content)
-        camaras.extend(matches)
+    camara = r'"primaryFromProduct": \["(.*?)"\]'
+    matches = re.findall(camara, content)
+    camaras.extend(matches)
 
-    objetos = []
-    for marca, nombre, precio, almacenamiento, pantalla, camara in zip(marcas, nombres, precios, almacenamientos,
-                                                                       pantallas, camaras):
-        objeto = {
-            "marca": marca,
-            "nombre": nombre,
-            "precio": precio,
-            "almacenamiento": almacenamiento,
-            "pantalla": pantalla,
-            "camara": camara
-        }
-        objetos.append(objeto)
+objetos = []
+for marca, nombre, precio, almacenamiento, pantalla, camara in zip(marcas, nombres, precios, almacenamientos,
+                                                                   pantallas, camaras):
+    objeto = {
+        "marca": marca,
+        "nombre": nombre,
+        "precio": precio,
+        "almacenamiento": almacenamiento,
+        "pantalla": pantalla,
+        "camara": camara
+    }
+    objetos.append(objeto)
 
-    for objeto in objetos:
-        json_objeto = json.dumps(objeto)
-        print(json_objeto)
-
-else:
-    print('No sé encontró el elemento con id "rootProductInfo_JSON"')
+with open(archivo_csv, mode='w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=headers)
+    writer.writeheader()
+    writer.writerows(objetos)
