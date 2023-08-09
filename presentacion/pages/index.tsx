b04ec45bc940bee7e20db422dd1a2874bc0d8842
Natalia Romero <natalia.romero_g@mail.udp.cl>
2023-08-09 12:14:35 -0400

    se cambian respuestas, idioma y se quitan sources de la visual

--- presentacion/pages/index.tsx
+++ presentacion/pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   }>({
     messages: [
       {
-        message: 'Hi, what would you like to learn about this document?',
+        message: '¡Hola! Bienvenido al chat de Entel. ¿En que te puedo ayudar?',
         type: 'apiMessage',
       },
     ],
@@ -125,7 +125,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat With Your Docs
+          Chat Entel 📱
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -174,7 +174,7 @@ export default function Home() {
                           </ReactMarkdown>
                         </div>
                       </div>
-                      {message.sourceDocs && (
+                      {/* {message.sourceDocs && (
                         <div
                           className="p-5"
                           key={`sourceDocsAccordion-${index}`}
@@ -203,7 +203,7 @@ export default function Home() {
                             ))}
                           </Accordion>
                         </div>
-                      )}
+                      )} */}
                     </>
                   );
                 })}
@@ -223,8 +223,8 @@ export default function Home() {
                     name="userInput"
                     placeholder={
                       loading
-                        ? 'Waiting for response...'
-                        : 'What is this legal case about?'
+                        ? 'Eperando respuesta...'
+                        : '¿En que puedo ayudarte hoy?'
                     }
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
@@ -261,9 +261,9 @@ export default function Home() {
           </main>
         </div>
         <footer className="m-auto p-4">
-          <a href="https://twitter.com/mayowaoshin">
+          {/* <a href="https://twitter.com/mayowaoshin">
             Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
-          </a>
+          </a> */}
         </footer>
       </Layout>
     </>
